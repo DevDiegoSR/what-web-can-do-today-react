cdd4cebd7a3dc0fc3ba95e035aee5c39408b6a4d
Diego dos Santos Rodrigues <diego.rodrigues@optimusgis.com.br>
2024-06-25 21:35:21 -0300

    watchHeading

--- src/pages/geolocation.tsx
+++ src/pages/geolocation.tsx
@@ -1,8 +1,14 @@
+import { Map } from "@/components/rol/map";
+import { OpenStreetMap } from "@/components/rol/sources";
+import { TileLayer } from "@/components/rol/tile-layer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 import { useGetCurrentPosition } from "@/hooks/geolocation/useGetCurrentPosition";
 import { useWatchPosition } from "@/hooks/geolocation/useWatchPosition";
+import { useContext, useEffect, useRef, useState } from "react";
+import { MapContext } from "@/components/rol/map-context";
+import { GeolocationMarker } from "@/components/rol/controls/geolocation/utils/GeolocationMarker";
 
 function GetCurrentPosition() {
   const { position, error, getCurrentPosition, clearCurrentPosition } =
@@ -42,6 +48,13 @@ function GetCurrentPosition() {
 }
 
 function WatchPosition() {
+  const map = useContext(MapContext);
+
+  const [geolocationMarker, setGeolocationMarker] =
+    useState<GeolocationMarker>();
+  const isMarkerFirstRenderRef = useRef(true);
+  const isFirstRenderRef = useRef(true);
+
   const { position, error, watcher, startWatchPosition, stopWatchPosition } =
     useWatchPosition();
 
@@ -50,6 +63,37 @@ function WatchPosition() {
     ? "Start Watch Position"
     : "Stop Watch Position";
   const buttonOnClick = hasWatcher ? startWatchPosition : stopWatchPosition;
+
+  useEffect(() => {
+    if (!map) return;
+    setGeolocationMarker(new GeolocationMarker(map));
+  }, [map]);
+
+  useEffect(() => {
+    if (position?.coords) {
+      if (isMarkerFirstRenderRef.current && !isFirstRenderRef.current) {
+        // first render add marker
+        isMarkerFirstRenderRef.current = false;
+        geolocationMarker?.addGeolocationMarker(position.coords);
+        return;
+      }
+
+      // other renders (update location) update marker
+      geolocationMarker?.updateGeolocationMarker(position.coords, false);
+      geolocationMarker?.setGeolocationMarkerRotation(
+        position.coords.heading ?? 0,
+        0
+      );
+      return;
+    }
+
+    isFirstRenderRef.current = false;
+    isMarkerFirstRenderRef.current = true;
+
+    return () => {
+      geolocationMarker?.setGeolocationMarkerRotation(0, 0);
+    };
+  }, [position?.coords]);
 
   return (
     <Card>
@@ -80,7 +124,17 @@ export function Geolocation() {
   return (
     <section className="flex-1 p-6 grid grid-cols-1 auto-rows-min gap-4">
       <GetCurrentPosition />
-      <WatchPosition />
+      <main className="h-screen w-full flex">
+        <Map
+          id="map-1"
+          className="flex-1"
+          zoom={12}
+          center={[-45.967823, -23.300423]}
+        >
+          <WatchPosition />
+          <TileLayer source={OpenStreetMap} zIndex={0} />
+        </Map>
+      </main>
     </section>
   );
 }
